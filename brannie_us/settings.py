f3b7523144eb38639b3cda60a63051b6217c16de
plancher <plancherb1@gmail.com>
2016-07-18 00:11:39 -0400

    some more basic setup stuff

--- brannie_us/settings.py
+++ brannie_us/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'version1',
+#    'south',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'brannie_us.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'django_db',
+	'USER': 'root',
+	'PASSWORD': 'cheese',
     }
 }
 
