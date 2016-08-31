b6c1862ef8573019400ab380408f3f78a977e432
Brian Plancher <plancherb1@gmail.com>
2016-08-30 23:47:04 -0400

    Update install.sh

--- install-tools/install.sh
+++ install-tools/install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # basic installs
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
-sudo apt-get install python-pip
-sudo apt-get install apache2
-sudo apt-get install apache2-dev
-sudo apt-get install python2.7-dev
-sudo apt-get install libapache2-mod-wsgi
-sudo pip install MySQL-python
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install git
+sudo apt-get -y install python-pip
+sudo apt-get -y install apache2
+sudo apt-get -y install apache2-dev
+sudo apt-get -y install python2.7-dev
+sudo apt-get -y install libapache2-mod-wsgi
+yes | sudo pip install MySQL-python
 
 # Uninstall old django by typing in
 # python -c "import django; print(django.__path__)"
 # Then cd to that directory and delete the django folder
 
 # Install django 1.9.7 (known to be stable)
-sudo pip install -U django==1.9.7
+yes | sudo pip install -U django==1.9.7
 # Alternatively drop the -u and the ==1.9.7 and try the latest
 
 # Download the website
