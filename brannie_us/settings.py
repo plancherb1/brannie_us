fa5b2fbdb36c5318520916362f6a27bf824dc708
Brian Plancher <Plancherb1@gmail.com>
2017-02-15 21:32:35 +0000

    need debug for images right now

--- brannie_us/settings.py
+++ brannie_us/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8c(efopq*j&w8nbqp)b0#hz6-4o34z822)u9bh=c!to3^(6*1a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
